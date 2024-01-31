99fe0406f826663c33240e9a8e8e6ed65b7f49a6
Anthony Villegas <villegasanthony87@yahoo.com>
2024-01-31 00:03:39 -0600

    connection for db set

---
+++ src/Conn.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+public class Conn {
+    Connection connection;
+    Statement statement;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql:///ebs", );
+            statement = connection.createStatement();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+    }
+}
